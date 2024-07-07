26cb7030cdf702d582a769b2db487f25b638cc71
na-talia <ntsh_89@ukr.net>
2024-07-07 23:51:04 +0200

    add input field

--- src/MyInput.jsx
+++ src/MyInput.jsx
@@ -3,10 +3,16 @@ import React, { useEffect, useRef, useState } from "react";
 const MyInput = () => {
   const [value, setValue] = useState(0);
   const count = useRef(0);
+  const inputElement = useRef();
 
   useEffect(() => {
     count.current++;
   }, [value]);
+
+  const btnClicked = () => {
+    console.log(inputElement.current);
+    inputElement.current.style.background = "red"; // change color when button is clicked
+  };
 
   return (
     <div>
@@ -14,6 +20,12 @@ const MyInput = () => {
       <h2>{value}</h2>
       <button onClick={() => setValue((prev) => prev - 1)}>-1</button>
       <h2>Render {count.current} times</h2>
+      <input
+        className="border border-gray-300 "
+        type="text"
+        ref={inputElement}
+      />
+      <button onClick={btnClicked}>Click me</button>
     </div>
   );
 };
